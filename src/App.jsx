c0bf77cb9852a524525236d777d37e2f057775c8
shlomohal <stairwaytoheaven84@gmail.com>
2025-08-11 20:50:23 +0300

    sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, HashRouter as Router, Routes, Navigate } from "react-router";
+import { Route, BrowserRouter as Router, Routes, Navigate } from "react-router";
 import { useEffect, useState } from "react";
 import GlobalContext from "./helpers/context/app.context";
 
